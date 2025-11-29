e2f9e75c7849c83c81c8056a19c724a57f2cdb68
HEMA-SWARUPA <hemaswarupagondela@gmail.com>
2025-11-29 20:01:24 +0530

    index

---
+++ script/admin.js
@@ -0,0 +1,6 @@
+let area=document.getElementsByTagName("main");
+function addCard(){
+let card=document.createElement("div");
+card.innerHTML
+   
+}
--- script/index.js
+++ script/index.js
@@ -1,9 +1,11 @@
-let emailva=document.getElementById("email").value.trim();
-let passwordva=document.getElementById("password").value.trim();
-let btn=document.getElementById("btn");
-btn.addEventListener("click",function(){
+
+var btn=document.getElementById("btn");
+btn.addEventListener("submit",function(){
+var emailva=document.getElementById("email").value;
+var passwordva=document.getElementById("password").value;
 if(emailva==="admin@gmail.com" && passwordva==="admin1234"){
     alert("Login success");
+    window.location.href="admin.html";
 }
 else{
     alert("wrong email or password");
