34fed548be1722922f02aa8f848a89bf2142124b
HEMA-SWARUPA <hemaswarupagondela@gmail.com>
2025-11-29 20:35:41 +0530

    inde1x

--- script/index.js
+++ script/index.js
@@ -5,7 +5,6 @@ var emailva=document.getElementById("email").value;
 var passwordva=document.getElementById("password").value;
 if(emailva==="admin@gmail.com" && passwordva==="admin1234"){
     alert("Login success");
-    window.location.href="admin.html";
 }
 else{
     alert("wrong email or password");
